30b215f9828c60c374bce6434a07fc9b5d66244c
kimsubin <subibi21@inu.ac.kr>
2024-05-31 23:00:40 +0900

    init commit

---
+++ src/main/java/com/example/techeerbackend/TecheerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.techeerbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TecheerBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TecheerBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/techeerbackend/TecheerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.techeerbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TecheerBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
